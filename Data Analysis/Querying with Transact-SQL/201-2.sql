e2246adc4eba06eabb7d5b3fc721116e47cbadf4
na1iu <nathan.f.liu@hotmail.com>
2017-03-02 17:47:06 +0800

    updates

--- Data Analysis/Querying with Transact-SQL/201-2.sql
+++ Data Analysis/Querying with Transact-SQL/201-2.sql
@@ -12,3 +12,20 @@ select name, listprice from saleslt.product order by productnumber offset 0 rows
 
 --Display the next ten products by product number
 select name, listprice from saleslt.product order by productnumber offset 10 rows fetch first 10 rows only;
+
+--Filtering and Using Predicates
+select * from saleslt.product
+--List information about product model 6
+select name, color, size from saleslt.product where productmodelid = 6;
+
+--List information about products that have a product number beginning FR
+select productnumber, name, color, size from saleslt.product where productnumber like 'FR%';
+
+--Filter the previous query to ensure that the product number contains two sets of two didgets
+select productnumber, name, color, size from saleslt.product where productnumber like 'FR-_[0-9][0-9]_-[0-9][0-9]';
+
+--Find products that have no sell end date
+select name, color, size, sellenddate from saleslt.product where sellenddate is not null;
+
+--Find products that have a sell end date in 2006
+select name, color, size, sellenddate from saleslt.product where sellenddate between '2006/1/1' and '2006/12/31';
